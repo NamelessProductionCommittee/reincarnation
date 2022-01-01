9e7ddff72f5a5261f98898878ab76d28768f07b6
teletha <485441+teletha@users.noreply.github.com>
2022-01-01 20:42:14 +0900

    Update JavascriptTestSupportTest.java

--- src/test/java/reincarnation/transpiler/js/JavascriptTestSupportTest.java
+++ src/test/java/reincarnation/transpiler/js/JavascriptTestSupportTest.java
@@ -12,7 +12,6 @@ package reincarnation.transpiler.js;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-@Disabled
 class JavascriptTestSupportTest extends JavascriptTestSupport {
 
     @Test
